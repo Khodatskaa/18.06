474a3b979d531215c17f9ccbd7c493cfc2e5cb79
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-07-02 13:24:29 +0300

    Ex 2 was done

--- 18.06/Client.cs
+++ 18.06/Client.cs
@@ -5,5 +5,29 @@ using System.Text;
 
 namespace _18._06
 {
-    
+    public class Client
+    {
+        static void Main(string[] args)
+        {
+            string serverIp = "127.0.0.1";
+            int port = 8080;
+
+            Console.WriteLine("Enter 'date' to request the current date or 'time' to request the current time:");
+            string request = Console.ReadLine().ToLower();
+
+            TcpClient client = new TcpClient(serverIp, port);
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = Encoding.UTF8.GetBytes(request);
+            stream.Write(buffer, 0, buffer.Length);
+
+            buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string responseMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine($"Received: {responseMessage}");
+
+            client.Close();
+        }
+    }
 }
--- 18.06/Program.cs
+++ 18.06/Program.cs
@@ -5,5 +5,46 @@ using System.Text;
 
 namespace _18._06
 {
-    
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            int port = 8080;
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+
+            TcpListener listener = new TcpListener(ipAddress, port);
+            listener.Start();
+
+            Console.WriteLine($"Server started at {DateTime.Now.ToString("HH:mm")} on {ipAddress}:{port}");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string request = Encoding.UTF8.GetString(buffer, 0, bytesRead).ToLower();
+
+                string responseMessage;
+                if (request == "date")
+                {
+                    responseMessage = DateTime.Now.ToString("yyyy-MM-dd");
+                }
+                else if (request == "time")
+                {
+                    responseMessage = DateTime.Now.ToString("HH:mm:ss");
+                }
+                else
+                {
+                    responseMessage = "Invalid request";
+                }
+
+                byte[] responseBuffer = Encoding.UTF8.GetBytes(responseMessage);
+                stream.Write(responseBuffer, 0, responseBuffer.Length);
+               
+                client.Close();
+            }
+        }
+    }
 }
