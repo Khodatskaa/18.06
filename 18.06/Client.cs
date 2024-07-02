ea85366bb4dd5869ed7ebdd73100a94921343f58
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-07-02 13:25:57 +0300

    Ex 3 was done

--- 18.06/Client.cs
+++ 18.06/Client.cs
@@ -5,5 +5,26 @@ using System.Text;
 
 namespace _18._06
 {
-    
+    public class Client
+    {
+        static async Task Main(string[] args)
+        {
+            string serverIp = "127.0.0.1";
+            int port = 8080;
+
+            using TcpClient client = new TcpClient();
+            await client.ConnectAsync(serverIp, port);
+            NetworkStream stream = client.GetStream();
+
+            string message = "Hello, server!";
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(buffer, 0, buffer.Length);
+
+            buffer = new byte[1024];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string responseMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine($"At {DateTime.Now:HH:mm} from {serverIp} received the line: {responseMessage}");
+        }
+    }
 }
--- 18.06/Program.cs
+++ 18.06/Program.cs
@@ -5,5 +5,38 @@ using System.Text;
 
 namespace _18._06
 {
-    
+    public class Server
+    {
+        static async Task Main(string[] args)
+        {
+            int port = 8080;
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            TcpListener listener = new TcpListener(ipAddress, port);
+            listener.Start();
+
+            Console.WriteLine($"Server started at {DateTime.Now:HH:mm} on {ipAddress}:{port}");
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                _ = HandleClientAsync(client);
+            }
+        }
+
+        private static async Task HandleClientAsync(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine($"At {DateTime.Now:HH:mm} from {((IPEndPoint)client.Client.RemoteEndPoint).Address} received the line: {message}");
+
+            string responseMessage = "Hello, client!";
+            byte[] responseBuffer = Encoding.UTF8.GetBytes(responseMessage);
+            await stream.WriteAsync(responseBuffer, 0, responseBuffer.Length);
+
+            client.Close();
+        }
+    }
 }
