511913a5613805e4db10d2f8efce0088bdc729d9
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-07-02 13:20:49 +0300

    Ex 1 was done

--- 18.06/Client.cs
+++ 18.06/Client.cs
@@ -5,5 +5,27 @@ using System.Text;
 
 namespace _18._06
 {
-    
+    public class Client
+    {
+        static void Main(string[] args)
+        {
+            string serverIp = "127.0.0.1";
+            int port = 8080;
+
+            TcpClient client = new TcpClient(serverIp, port);
+            NetworkStream stream = client.GetStream();
+
+            string message = "Hello, server!";
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+            stream.Write(buffer, 0, buffer.Length);
+
+            buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string responseMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine($"At {DateTime.Now.ToString("HH:mm")} from {serverIp} received the line: {responseMessage}");
+
+            client.Close();
+        }
+    }
 }
--- 18.06/Program.cs
+++ 18.06/Program.cs
@@ -5,5 +5,33 @@ using System.Text;
 
 namespace _18._06
 {
-    
+    public class Server
+    {
+        static void Main(string[] args)
+        {
+            int port = 8080;
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+
+            TcpListener listener = new TcpListener(ipAddress, port);
+            listener.Start();
+
+            Console.WriteLine($"Server started at {DateTime.Now.ToString("HH:mm")} on {ipAddress}:{port}");
+
+            TcpClient client = listener.AcceptTcpClient();
+            NetworkStream stream = client.GetStream();
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+            Console.WriteLine($"At {DateTime.Now.ToString("HH:mm")} from {((IPEndPoint)client.Client.RemoteEndPoint).Address} received the line: {message}");
+
+            string responseMessage = "Hello, client!";
+            byte[] responseBuffer = Encoding.UTF8.GetBytes(responseMessage);
+            stream.Write(responseBuffer, 0, responseBuffer.Length);
+
+            client.Close();
+            listener.Stop();
+        }
+    }
 }
